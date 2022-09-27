7ca0b6ac84af62c6eb449ec52c439a363b9318d2
gghotted <gghotted2@naver.com>
2022-09-27 18:25:34 +0900

    update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='django_message',
     version='0.0.0',
-    packages=['django_message'],
+    packages=find_packages(exclude=['demo*']),
     install_requires=[
         'django>=3.5',
     ]
